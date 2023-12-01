fe7a791c0d734c5d2b0792107ae27fd5c96afb25
mmatinr <152607984+mmatinr@users.noreply.github.com>
2023-12-01 14:15:46 +0330

    Add files via upload
    hw week 4 matin rasouli

---
+++ hw week 4 part 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	float m,n,s;
+
+cout <<"enter your first number";
+cin >> m;
+cout << "enter your second number" ;
+cin >> n;	   
+    s=m+n ;
+	s=s-n ;
+	s=s*n ;
+	s=s/n ;
+	cout<< s << "is your result";
+	
+}
---
+++ hw week 4 part 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+main()  { 
+
+int x,y,z,m,n,s,a;
+cout<< "enter the value for x";
+cin>> x;
+cout<< "enter the value for y";
+cin>> y;
+cout<< "enter the value for z";
+cin>> z;
+m=x-y;
+n=x+z; 
+s=x+y+z;
+s=s*s;
+s++ ;
+m=m*n;
+a=m/s; 
+cout<< a; "is your answer";
+}
